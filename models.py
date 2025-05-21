dc7c2fd22fb2f5762239aceac0e0477795796231
Álvaro Thees <alvaro.thees@levty.com>
2025-05-21 16:43:37 -0300

    Database Models

---
+++ models.py
@@ -0,0 +1,24 @@
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, create_engine, Date
+from sqlalchemy.orm import relationship, sessionmaker, declarative_base
+
+Base = declarative_base()
+
+class ToDoList(Base):
+    __tablename__ = 'todo_lists'
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    items = relationship("ToDoItem", backref="todo_list", cascade="all, delete")
+
+class ToDoItem(Base):
+    __tablename__ = 'todo_items'
+    id = Column(Integer, primary_key=True)
+    content = Column(String, nullable=False)
+    date = Column(Date)
+    completed = Column(Boolean, default=False)
+    list_id = Column(Integer, ForeignKey('todo_lists.id'), nullable=False)
+
+
+# Conexão com SQLite
+engine = create_engine('sqlite:///todo.db')
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
